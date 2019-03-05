7683fd803036770cb38995e6885daa65282e48e2
Jennifer Cafiero <jcafiero@stevens.edu>
2019-03-05 15:09:28 -0500

    makes changes to program to fix coverage and pass linter

--- test_triangle.py
+++ test_triangle.py
@@ -8,8 +8,18 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 """
 
 import unittest
+# -*- coding: utf-8 -*-
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
 
-from Triangle import classifyTriangle
+@author: jrr
+@author: rk
+"""
+
+import unittest
+
+from triangle import classify_triangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -18,36 +28,35 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classify_triangle(3,4,5),'Right','3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classify_triangle(5,3,4),'Right','5,3,4 is a Right triangle')
 
     def testRightTriangleC(self):
-        self.assertEqual(classifyTriangle(5,12,13),'Right','5,12,13 is a Right triangle')
+        self.assertEqual(classify_triangle(5,12,13),'Right','5,12,13 is a Right triangle')
         
     def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-        self.assertEqual(classifyTriangle(12,12,12),'Equilateral','12,12,12 should be equilateral')
+        self.assertEqual(classify_triangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classify_triangle(12,12,12),'Equilateral','12,12,12 should be equilateral')
     
     def testIsoscelesTriangles(self):
-        self.assertEqual(classifyTriangle(4,4,3),'Isosceles','4,4,3 should be isosceles')
-        self.assertEqual(classifyTriangle(9,10,10),'Isosceles','9,10,10 should be isosceles')
+        self.assertEqual(classify_triangle(4,4,3),'Isosceles','4,4,3 should be isosceles')
+        self.assertEqual(classify_triangle(9,10,10),'Isosceles','9,10,10 should be isosceles')
 
     def testScaleneTriangles(self):
-        self.assertEqual(classifyTriangle(5,6,8),'Scalene','5,6,8 should be scalene')
-        self.assertEqual(classifyTriangle(2,6,7),'Scalene','2,6,7 should be scalene')
+        self.assertEqual(classify_triangle(5,6,8),'Scalene','5,6,8 should be scalene')
+        self.assertEqual(classify_triangle(2,6,7),'Scalene','2,6,7 should be scalene')
 
     def testNotATriangle(self):
-        self.assertEqual(classifyTriangle(1,1,2),'NotATriangle','1,1,2 is not a triangle')
-        self.assertEqual(classifyTriangle(4,4,20),'NotATriangle','4,4,20 is not a triangle')
+        self.assertEqual(classify_triangle(1,1,2),'NotATriangle','1,1,2 is not a triangle')
+        self.assertEqual(classify_triangle(4,4,20),'NotATriangle','4,4,20 is not a triangle')
 
     def testValidInput(self):
-        self.assertEqual(classifyTriangle(500,500,500),'InvalidInput','500,500,500 is invalid input')
-        self.assertEqual(classifyTriangle('250',25,25),'InvalidInput',"'250',250,250 is invalid input")
-        self.assertEqual(classifyTriangle(-5,3,4),'InvalidInput','-5,3,4 is invalid input')
+        self.assertEqual(classify_triangle(500,500,500),'InvalidInput','500,500,500 is invalid input')
+        self.assertEqual(classify_triangle('250',25,25),'InvalidInput',"'250',250,250 is invalid input")
+        self.assertEqual(classify_triangle(-5,3,4),'InvalidInput','-5,3,4 is invalid input')
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
--- triangle.py
+++ triangle.py
@@ -9,49 +9,54 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+def classify_triangle(side_a, side_b, side_c):
     """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
+    Your correct code goes here...  Fix the faulty logic below until the code passes all of
+    you test cases.
+
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
-    
+
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
+
       BEWARE: there may be a bug or two in this code
     """
-    # verify that all 3 inputs are integers  
+    output = ''
+    # verify that all 3 inputs are integers
     # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+    if not isinstance(side_a, int) or not isinstance(side_b, int) or not isinstance(side_c, int):
+        output = 'InvalidInput'
 
     # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
-        return 'InvalidInput'
+    elif side_a > 200 or side_b > 200 or side_c > 200:
+        output = 'InvalidInput'
+
+    elif side_a <= 0 or side_b <= 0 or side_c <= 0:
+        output = 'InvalidInput'
         
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
-    
-      
-    # This information was not in the requirements spec but 
+    if output != '':
+        return output
+
+    # This information was not in the requirements spec but
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+    if (side_a >= (side_b + side_c)) or (side_b >= (side_a + side_c)) or \
+        (side_c >= (side_a + side_b)):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if a == b and a == c:
+
+    # now we know that we have a valid triangle
+    if side_a == side_b and side_a == side_c:
         return 'Equilateral'
-    elif (((a ** 2) + (b ** 2)) == (c ** 2)) or (((a ** 2) + (c ** 2)) == (b ** 2)) or (((c ** 2) + (b ** 2)) == (a ** 2)):
+    if (((side_a ** 2) + (side_b ** 2)) == (side_c ** 2)) or (((side_a ** 2) + \
+        (side_c ** 2)) == (side_b ** 2)) or (((side_c ** 2) + (side_b ** 2)) == (side_a ** 2)):
         return 'Right'
-    elif (a != b and b != c) and  (b != c and a != c) and (a != b and a != c):
+    if (side_b not in (side_a, side_c)) and (side_c not in (side_a, side_b)) and \
+        (side_a not in (side_b, side_c)):
         return 'Scalene'
-    else:
-        return 'Isosceles'
+    return 'Isosceles'
